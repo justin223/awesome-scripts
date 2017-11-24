3454ea021b27002d5c5aebb6705f4137e13f75b7
Justin Tang <Justin.ZT.Tang@gmail.com>
2017-11-24 14:26:41 +0800

    updated Linux/backdb.sh.

--- Linux/bash/backdb.sh
+++ Linux/bash/backdb.sh
@@ -13,10 +13,23 @@ DATETIME=`date +"%Y%m%d%H%M"`
 # Do the work
 for i in "${DB_NAME[@]}"
 do
-mysqldump --user=$DBUSER --password=$DBPWD --host=$HOST $i | gzip > $BACKUP_DIR/bak_$i_$DATETIME.sql.gz
+mysqldump --user=$DBUSER --password=$DBPWD --host=$HOST $i | gzip > $BACKUP_DIR/MySQL_$i_$DATETIME.sql.gz
 done
+
+# TODO Upload backups to external storage, e.g., NAS, FTP or NFS server
+mkdir /mnt/cptemp
+mount -t nfs 1.2.3.4:/nfsshare /mnt/cptemp
+# or CIFS share
+# mount -t cifs -o username=user,password=pass,domain=example.com //1.2.3.4/cifsshare /mnt/cptemp
+if [ $? -eq 0]; then
+    for i in "${DB_NAME[@]}"
+    do
+        cp $BACKUP_DIR/MySQL_$i_$DATETIME.sql.gz /mnt/cptemp
+    done
+    umount /mnt/cptemp
+else
+    echo "Mount error: $?"
+fi
 
 # Remove the ones older than 3 days
 find $BACKUP_DIR/ -type f -name *.sql.gz -mtime +3 -exec rm {} \;
-
-# TODO Upload backups to external storage, e.g., NAS, FTP or NFS server
