c0a7239636950bfb4c1dfd6369d9a3838e788df3
Justin Tang <Justin.ZT.Tang@gmail.com>
2017-11-24 17:00:06 +0800

    rename files.

---
+++ Linux/bash/back_mysql.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Database credentials
+DBUSER='blah'
+DBPWD='blahblahblah'
+HOST='127.0.0.1'
+DB_NAME=(db1 db2)
+
+# Other options
+BACKUP_DIR='/path/to/your/backup/dir'
+DATETIME=`date +"%Y%m%d%H%M"`
+MYSQLDUMP=$(which mysqldump)
+GZIP=$(which gzip)
+
+# Do the work
+for i in "${DB_NAME[@]}"
+do
+    $MYSQLDUMP --user=$DBUSER --password=$DBPWD --host=$HOST $i | $GZIP > $BACKUP_DIR/MySQL_$i_$DATETIME.sql.gz
+done
+
+# TODO Upload backups to external storage, e.g., NAS, FTP or NFS server
+mkdir /mnt/cptemp
+mount -t nfs 1.2.3.4:/nfsshare /mnt/cptemp
+# or CIFS share
+# mount -t cifs -o username=user,password=pass,domain=example.com //1.2.3.4/cifsshare /mnt/cptemp
+if [ $? -eq 0]; then
+    for i in "${DB_NAME[@]}"
+    do
+        cp $BACKUP_DIR/MySQL_$i_$DATETIME.sql.gz /mnt/cptemp
+    done
+    umount /mnt/cptemp
+else
+    echo "Mount error: $?"
+fi
+
+# Remove the ones older than 3 days
+find $BACKUP_DIR/ -type f -name *.sql.gz -mtime +3 -exec rm {} \;
