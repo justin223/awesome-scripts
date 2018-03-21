40606c7df903097963209f5f27eb0a637a4202ac
Justin Tang <Justin.ZT.Tang@gmail.com>
2018-03-21 16:20:34 +0800

    correct redirection fd

--- macOS/deluser.sh
+++ macOS/deluser.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # Filter AD users 80xxxx into file users.txt
-dscl . list /Users GeneratedUID | grep -v ^_.*  | grep ^80 | awk '{print $1}' > users.txt
+dscl . list /Users | grep -v ^_.*  | grep ^80 > users.txt
 
 # Read from file users.txt line by line, and remove it
 while read -r line
 do
 	uid="$line"
 	echo $uid
-	sysadminctl -deleteUser $uid -secure > null 2>&1
+	# sysadminctl -deleteUser $uid -secure > /dev/null 2>&1
+	dscl . delete /Users/$uid > /dev/null 2>&1
+	rm -rf /Users/$uid
 done < users.txt
 
 # Remove user with ID $1 (the first parameter)
 USERID=`dscl . list /Users GeneratedUID | grep -v ^_.*  | grep $1 | awk '{print $1}'`
 echo $USERID
-sysadminctl -deleteUser $USERID -secure > null 2>&1
+sysadminctl -deleteUser $USERID -secure > /dev/null 2>&1
