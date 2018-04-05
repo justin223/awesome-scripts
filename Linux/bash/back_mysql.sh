75b97412e1624c64c506510c81906272bef6b353
Justin Tang <Justin.ZT.Tang@gmail.com>
2018-04-05 12:09:25 +0800

    update logic

--- Linux/bash/back_mysql.sh
+++ Linux/bash/back_mysql.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Database credentials
-DBUSER='blah'
-DBPWD='blahblahblah'
-HOST='127.0.0.1'
-DB_NAME=(db1 db2)
+DBUSER=blah
+DBPWD=blahblahblah
+HOST=127.0.0.1
+DB_NAME="db1 db2"
 
 # Other options
 BACKUP_DIR='/path/to/your/backup/dir'
@@ -13,9 +13,11 @@ MYSQLDUMP=$(which mysqldump)
 GZIP=$(which gzip)
 
 # Do the work
-for i in "${DB_NAME[@]}"
+for i in $DB_NAME
 do
-    $MYSQLDUMP --user=$DBUSER --password=$DBPWD --host=$HOST $i | $GZIP > $BACKUP_DIR/MySQL_$i_$DATETIME.sql.gz
+    $MYSQLDUMP --user=$DBUSER --password=$DBPWD --host=$HOST $i > "$BACKUP_DIR/MySQL_$i-$DATETIME.sql" 
+#    $GZIP "$BACKUP_DIR/MySQL_$i-$DATETIME.sql" > $BACKUP_DIR/MySQL_$i_$DATETIME.sql.gz
+    $GZIP "$BACKUP_DIR/MySQL_$i-$DATETIME.sql" 
 done
 
 # Upload backups to external storage, e.g., NAS, FTP or NFS server
@@ -24,9 +26,9 @@ mount -t nfs 1.2.3.4:/nfsshare /mnt/cptemp
 # or CIFS share
 # mount -t cifs -o username=user,password=pass,domain=example.com //1.2.3.4/cifsshare /mnt/cptemp
 if [ $? -eq 0]; then
-    for i in "${DB_NAME[@]}"
+    for i in $DB_NAME
     do
-        cp $BACKUP_DIR/MySQL_$i_$DATETIME.sql.gz /mnt/cptemp
+        cp $BACKUP_DIR/MySQL_$i-$DATETIME.sql.gz /mnt/cptemp
     done
     umount /mnt/cptemp
 else
